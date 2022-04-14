cae0e14494c8ac5a6dfd9d387db8d1a0a0a2ee0b
ysenkun <82140392+ysenkun@users.noreply.github.com>
2022-04-14 19:17:57 +0900

    Update register.sh

--- facenet_mask/facenet/register.sh
+++ facenet_mask/facenet/register.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 
 model='../facenet_model/vggface2/20180402-114759.pb'
-
-#image1='data/images/sentaro001.jpg'
-# image1='./src/data/images/sentaro001.jpg'
-# image2='./src/data/images/sentaro002.jpg'
-#image1='./src/data/images/kari.001.jpg'
-image1='./src/data/images/iuchi.001.jpg'
-file='image'
-
-#/opt/anaconda3/envs/mask/bin/python3 src/create_register.py ${model} ${image1} ${image2} --image_size 160 --margin 32 --gpu_memory_fraction 0
+image1='./src/data/images/name.001.jpg'
 
 /opt/anaconda3/envs/mask/bin/python3 src/create_register.py ${model} ${image1} --image_size 160 --margin 32 --gpu_memory_fraction 0
