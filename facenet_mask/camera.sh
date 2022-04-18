2f80e4a4662452c5847057930de0457f989ebaba
ysenkun <sentaro.yoshida@cps.akita-pu.ac.jp>
2022-04-18 17:34:36 +0900

    modified shell script

--- facenet_mask/camera.sh
+++ facenet_mask/camera.sh
@@ -3,6 +3,6 @@
 model='facenet_model/vggface2/20180402-114759.pb'
 
 #conda activate facenet
-/opt/anaconda3/envs/facenet/bin/python facenet/src/camera_facemask.py ${model} --image_size 160 --margin 32 --gpu_memory_fraction 0.5
+{YOUR_APPROPRIATE_PATH}/facenet/bin/python facenet/src/camera_facemask.py ${model} --image_size 160 --margin 32 --gpu_memory_fraction 0.5
 
 
--- facenet_mask/facenet/create.sh
+++ facenet_mask/facenet/create.sh
@@ -8,7 +8,7 @@ ARR=`seq -s ' ' $num `
 for i in $ARR
 do
 	echo $i'人目'
-	/opt/anaconda3/envs/facenet/bin/python3 camera.py
+	{YOUR_APPROPRIATE_PATH}/facenet/bin/python3 camera.py
 done
 
 files="./src/data/images/*"
@@ -16,6 +16,6 @@ files="./src/data/images/*"
 rm register.db
 
 # conda activate facenet
-/opt/anaconda3/envs/facenet/bin/python3 src/create_register.py ${model} ${files} --image_size 160 --margin 32 --gpu_memory_fraction 0
+{YOUR_APPROPRIATE_PATH}/facenet/bin/python3 src/create_register.py ${model} ${files} --image_size 160 --margin 32 --gpu_memory_fraction 0
 
 rm ./src/data/images/*
